ba1f548dbc2d49b6a6ddc844e33af6e18c54be88
sandravce <sandravismaya@gmail.com>
2025-11-04 14:48:53 +0530

    Refactor single linked list structure and functions
    Refactor linked list implementation to use consistent struct naming and improve node management functions.

--- october 25/singlelinkedlist.c
+++ october 25/singlelinkedlist.c
@@ -1,200 +1,141 @@
-#include<stdio.h>
-#include<stdlib.h>
-// "node" template structure
-struct node
-{
-	int data;
-	struct node *link;
-};
-struct node *header;
-header=NULL;
-
-void createnode()
-{
-	struct node *newnode;
-	newnode=(struct node *)malloc(sizeof(struct node));
-	newnode->data= 0;
-	newnode->link=NULL;
-	header=newnode;
-}
-
-int main()
-{
-	createnode();
-	printf("Data:%d",);
-	return(0);
-}
-
-
-
-
-
-
-chatgpt code 👇👇
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a node
-struct Node {
+// Node structure
+struct node {
     int data;
-    struct Node *next;
+    struct node *link;
 };
 
-struct Node *head = NULL;
-
-// Create a new node
-struct Node* createNode(int value) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = value;
-    newNode->next = NULL;
-    return newNode;
-}
+struct node *header = NULL;
 
 // Insert at front
-void insertAtFront(int value) {
-    struct Node* newNode = createNode(value);
-    newNode->next = head;
-    head = newNode;
-    printf("Inserted %d at front.\n", value);
+void insertAtFront(int item) {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = item;
+    newnode->link = header;
+    header = newnode;
 }
+
+// Delete at front
+void deleteAtFront()
+{
+	struct node*ptr;
+	if (header == NULL)
+	{
+		printf("Deletion is not possible since there is no node.\n");
+	}
+	else
+	{
+		struct node *ptr;
+		ptr = header;
+		header = header->link;
+		free(ptr);
+	}
+}
+
 
 // Insert at end
-void insertAtEnd(int value) {
-    struct Node* newNode = createNode(value);
-    if (head == NULL) {
-        head = newNode;
+void insertAtEnd(int item) {
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = item;
+    newnode->link = NULL;
+
+    if (header == NULL) {
+        header = newnode;
     } else {
-        struct Node* temp = head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = newNode;
+        struct node *ptr = header;
+        while (ptr->link != NULL) {
+            ptr = ptr->link;
+        }
+        ptr->link = newnode;
     }
-    printf("Inserted %d at end.\n", value);
 }
 
-// Insert at given position (1-based index)
-void insertAtPosition(int value, int position) {
-    struct Node* newNode = createNode(value);
+// Delete at end 
+void deleteAtEnd()
+{
+	if(header == NULL)
+    	{
+        	printf("Deletion is not possible since there is no node.\n");
+    	}
+    	else
+    	{
+		struct node *ptr,*ptr1;
+		ptr=ptr1=header;
+		while(ptr->link!=NULL)
+		{
+			ptr=ptr1;
+			ptr=ptr->link;
+		}
+		printf("Deleted element is %d",ptr->data);
+		ptr->link=NULL;
+		free(ptr);
+	}
+}
 
-    if (position == 1) {
-        newNode->next = head;
-        head = newNode;
-        printf("Inserted %d at position %d.\n", value, position);
-        return;
+
+// Insert after a key
+void insertAtAny(int key, int item) {
+    struct node *ptr = header, *ptr1 = NULL, *newnode;
+
+    while (ptr != NULL && ptr->data != key) {
+        ptr1 = ptr;
+        ptr = ptr->link;
     }
 
-    struct Node* temp = head;
-    for (int i = 1; i < position - 1 && temp != NULL; i++)
-        temp = temp->next;
-
-    if (temp == NULL) {
-        printf("Position out of range!\n");
-        free(newNode);
+    if (ptr == NULL) {
+        printf("Key %d not found, insertion not possible.\n", key);
     } else {
-        newNode->next = temp->next;
-        temp->next = newNode;
-        printf("Inserted %d at position %d.\n", value, position);
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->data = item;
+        newnode->link = ptr->link;
+        ptr->link = newnode;
+        printf("Inserted %d after %d successfully\n", item, key);
     }
 }
 
-// Delete from front
-void deleteFromFront() {
-    if (head == NULL) {
-        printf("List is empty.\n");
-        return;
+//Delete at any 
+
+
+// Traverse and print
+void traverselist() {
+    struct node *ptr = header;
+    while (ptr != NULL) {
+        printf("%d\n", ptr->data);
+        ptr = ptr->link;
     }
-    struct Node* temp = head;
-    head = head->next;
-    printf("Deleted %d from front.\n", temp->data);
-    free(temp);
-}
-
-// Delete from end
-void deleteFromEnd() {
-    if (head == NULL) {
-        printf("List is empty.\n");
-        return;
-    }
-
-    if (head->next == NULL) {
-        printf("Deleted %d from end.\n", head->data);
-        free(head);
-        head = NULL;
-        return;
-    }
-
-    struct Node* temp = head;
-    while (temp->next->next != NULL)
-        temp = temp->next;
-
-    printf("Deleted %d from end.\n", temp->next->data);
-    free(temp->next);
-    temp->next = NULL;
-}
-
-// Delete from a given position
-void deleteFromPosition(int position) {
-    if (head == NULL) {
-        printf("List is empty.\n");
-        return;
-    }
-
-    struct Node* temp = head;
-
-    if (position == 1) {
-        head = head->next;
-        printf("Deleted %d from position %d.\n", temp->data, position);
-        free(temp);
-        return;
-    }
-
-    struct Node* prev = NULL;
-    for (int i = 1; i < position && temp != NULL; i++) {
-        prev = temp;
-        temp = temp->next;
-    }
-
-    if (temp == NULL) {
-        printf("Position out of range!\n");
-    } else {
-        prev->next = temp->next;
-        printf("Deleted %d from position %d.\n", temp->data, position);
-        free(temp);
-    }
-}
-
-// Display list
-void display() {
-    struct Node* temp = head;
-    if (temp == NULL) {
-        printf("List is empty.\n");
-        return;
-    }
-    printf("Linked List: ");
-    while (temp != NULL) {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
-    }
-    printf("NULL\n");
 }
 
 // Main function
-int main() {
-    // Insertions
-    insertAtFront(10);
-    insertAtEnd(20);
-    insertAtEnd(30);
-    insertAtPosition(15, 2);  // insert 15 at position 2
-    display();
+int main() 
+{
+	insertAtFront(10);
+	insertAtFront(20);
+	insertAtFront(30);
+	insertAtFront(40);
+	insertAtFront(50);
 
-    // Deletions
-    deleteFromFront();
-    deleteFromEnd();
-    deleteFromPosition(2);    // delete element at position 2
-    display();
+	printf("List after inserting at front:\n");
+	traverselist();
+
+	deleteAtFront();  
+	printf("List after deleting at front:\n");
+	traverselist();
+
+	printf("List after inserting at end:\n");
+	insertAtEnd(50);
+	traverselist();
+
+	printf("List after deleting at end :\n");
+	deleteAtEnd();
+	traverselist();
+		
+	printf("List after inserting after 30:\n");
+	insertAtAny(30, 45);
+	traverselist();
 
     return 0;
 }
+
 
